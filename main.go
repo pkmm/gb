f869a95edfa57b453c75f72e2cf3ec219fe9be24
pkmm <robotgg@126.com>
2017-11-18 13:31:58 +0800

    auto

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
 	//sample example
-	worker := baidu.NewForumWorker("your baidu bduss")
+	worker := baidu.NewForumWorker("01T1AyRG1XY28ydTRNbVFTbmQzM1pwOXVMbnk2cWo3ODV1eFh6bW1DWXZvUjVhTVFBQUFBJCQAAAAAAAAAAAEAAABS5n44vrLLvNSwNAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAC8U91kvFPdZcW")
 	data := baidu.ForumList{}
-	data = append(data, baidu.Forum{"贴吧名称", "贴吧fid"})
+	data = append(data, baidu.Forum{"acm", worker.GetFid("acm")})
 	ret := worker.SignAll(&data)
 	fmt.Print(*ret)
 }
